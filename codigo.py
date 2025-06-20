a0d8e610ee47ee8e5d434e55c378662dd8923c5b
hadesCode97 <luis.bonilla@estrellas.app>
2025-06-20 07:37:37 -0500

    feat(quicksort): quicksort algorithm implementation

--- codigo.py
+++ codigo.py
@@ -1,2 +1,12 @@
-for i in range(10):
-	print(i)
+def quickFilter(arr: list[int]):
+    if(len(arr) < 2):
+        return arr
+    else:
+        pivot: int = arr[0]
+        major = list(filter(lambda x: x > pivot, arr))
+        minor = list(filter(lambda x: x < pivot, arr))
+        return quickFilter(minor) + [pivot] + quickFilter(major)
+    
+arr: list[int] = [99, 98, 0, -1, 8]
+response: list[int] = quickFilter(arr)
+print(response)
